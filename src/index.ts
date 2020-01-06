be4f3e62d940584561b70614a75dc403a241c1e6
letsget23 <letsget23@gmail.com>
2020-01-06 23:47:56 +0900

    TypeScript configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+// import PeopleParser from './readfile/oop/PropleParser';
+
+// PeopleParser.getPeople()
+//   .then(console.log)
+//   .catch(console.error);
+
+import fs from 'fs';
+import { getPeople, getDefaultEncoding } from './readfile/fp';
+
+getPeople(fs)(getDefaultEncoding())(__dirname + '/readfile/people.txt')
+  .then(console.log)
+  .catch(console.error);
